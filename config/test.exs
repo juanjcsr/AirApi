4b428d45ee325f0f7435cc3d488e8fa81f81e756
Juan Carlos <juanjcsr@gmail.com>
2016-04-15 17:11:29 -0500

    default database users

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :air_api, AirApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "airapp",
+  password: "airapp",
   database: "air_api_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
