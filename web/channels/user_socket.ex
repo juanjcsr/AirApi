e87bcb6ea73f17be81d8a842075607d9a3841514
Juan Carlos <juanjcsr@gmail.com>
2016-04-23 23:44:48 -0500

    add chat in channel form

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule AirApi.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", AirApi.RoomChannel
+  channel "rooms:*", AirApi.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
