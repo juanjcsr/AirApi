059139fcb0b0b06a5e2e27e3768f9b96af961986
Juan Carlos <juanjcsr@gmail.com>
2016-05-19 19:26:42 -0500

    todo api finalizada en server

--- web/controllers/todo_controller.ex
+++ web/controllers/todo_controller.ex
@@ -6,7 +6,7 @@ defmodule AirApi.TodoController do
 
   plug :scrub_params, "todo" when action in [:create, :update]
   plug Guardian.Plug.EnsureAuthenticated, handler: __MODULE__
-
+  plug :correct_user when action in [:update, :show, :delete,:create]
   #plug AirApi.Authentication
 
   def index(conn, _params) do
@@ -15,21 +15,14 @@ defmodule AirApi.TodoController do
     query = from t in Todo, where: t.owner_id == ^user.id
     todos = Repo.all(query)
     render(conn, "index.json", todos: todos)
-    #IO.puts("TESTTT")
-    #IO.inspect(_params)
-    #user_id = conn.assigns.current_user.id
-    #query = from t in Todo, where: t.owner_id == ^user_id
-    #todos = Repo.all(query)
-
-    #render(conn, "index.json", todos: todos)
   end
 
   def create(conn, %{"todo" => todo_params}) do
-    user = Guardian.Plug.current_resource(conn)
-    changeset = Todo.changeset(%Todo{}, todo_params)
-    #changeset = Todo.changeset(
-    #  %Todo{owner_id: conn.assigns.current_user.id}, todo_params
-    #)
+    #user = Guardian.Plug.current_resource(conn)
+    #changeset = Todo.changeset(%Todo{}, todo_params)
+    changeset = Todo.changeset(
+      %Todo{owner_id: conn.assigns.user_id}, todo_params
+    )
     case Repo.insert(changeset) do
       {:ok, todo} ->
         conn
@@ -44,14 +37,39 @@ defmodule AirApi.TodoController do
   end
 
   def show(conn, %{"id" => id}) do
-    todo = Repo.get!(Todo, id)
-    render(conn, "show.json", todo: todo)
+    query = from t in Todo, where: t.owner_id == ^conn.assigns[:user_id] and t.id == ^id
+    case Repo.one(query) do
+      nil ->
+        conn
+        |> put_status(404)
+        |> render(AirApi.ErrorView, "404.json")
+      todo ->
+        render(conn, "show.json", todo: todo)
+
+    end
   end
 
   def update(conn, %{"id" => id, "todo" => todo_params}) do
-    IEx.pry
-    todo = Repo.get!(Todo, id)
-    changeset = Todo.changeset(todo, todo_params)
+    query = from t in Todo, where: t.owner_id == ^conn.assigns[:user_id] and t.id == ^id
+    case Repo.one(query) do
+      nil ->
+        conn
+        |> put_status(404)
+        |> render(AirApi.ErrorView, "404.json")
+      todo ->
+        changeset = Todo.changeset(todo, todo_params)
+        case Repo.update(changeset) do
+         {:ok, todo} ->
+           render(conn, "show.json", todo: todo)
+         {:error, changeset} ->
+           conn
+           |> put_status(:unprocessable_entity)
+           |> render(AirApi.ChangesetView, "error.json", changeset: changeset)
+        end
+
+    end
+    #changeset = Todo.changeset(todo, todo_params)
+
     #if(todo.owner_id == conn.assigns)
     #IO.inspect(todo)
     #case Repo.update(changeset) do
@@ -65,19 +83,28 @@ defmodule AirApi.TodoController do
   end
 
   def delete(conn, %{"id" => id}) do
-    todo = Repo.get!(Todo, id)
-
+    query = from t in Todo, where: t.owner_id == ^conn.assigns[:user_id] and t.id == ^id
+    case Repo.one(query) do
+       nil ->
+        conn
+        |> put_status(404)
+        |> render(AirApi.ErrorView, "404.json")
+      todo ->
+        send_resp(conn, :no_content, "")
+    end
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
-    Repo.delete!(todo)
-
-    send_resp(conn, :no_content, "")
   end
 
   def unauthenticated(conn, _params) do
     conn
     |> put_status(401)
     |> render AirApi.ErrorView, "unauthenticated.json", message: "Authentication required"
+  end
+
+  defp correct_user(conn, _) do
+    user = Guardian.Plug.current_resource(conn)
+    assign(conn, :user_id, user.id)
   end
 
 
